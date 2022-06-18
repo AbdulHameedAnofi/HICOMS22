0ca6433f5c77ac49db4d1493bc1168c9ed49b018
Abdulhameed Anofi <anofiolawale@gmail.com>
2022-06-18 10:17:50 +0100

    changed database again

--- Connection.php
+++ Connection.php
@@ -3,11 +3,16 @@
 
 function connection() {
     
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url['host'];
+    $cleardb_username = $cleardb_url['user'];
+    $cleardb_password = $cleardb_url['pass'];
+    $cleardb_db = substr($cleardb_url['path'], 1);
     $active_group = 'default';
     $query_builder = TRUE;
     // mysql://b790d3e52dd8c0:fbe64c51@eu-cdbr-west-02.cleardb.net/heroku_f658271a9b91c34?reconnect=true
         try {
-            $connection = new PDO("mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_f658271a9b91c34", 'b790d3e52dd8c0', 'fbe64c51');
+            $connection = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db", $cleardb_username, $cleardb_password);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $connection;
         } catch (PDOException $th) {
