6d797e3b86425268d9b66ff1c427a72570b84bcd
Abdulhameed Anofi <anofiolawale@gmail.com>
2022-06-18 12:01:24 +0100

    added phpinfo

--- Connection.php
+++ Connection.php
@@ -1,6 +1,9 @@
 <?php
 
 
+$active_group = 'default';
+$query_builder = TRUE;
+
 function connection() {
     
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -8,8 +11,6 @@ function connection() {
     $cleardb_username = $cleardb_url['user'];
     $cleardb_password = $cleardb_url['pass'];
     $cleardb_db = substr($cleardb_url['path'], 1);
-    $active_group = 'default';
-    $query_builder = TRUE;
     // mysql://b790d3e52dd8c0:fbe64c51@eu-cdbr-west-02.cleardb.net/heroku_f658271a9b91c34?reconnect=true
         try {
             $connection = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db", $cleardb_username, $cleardb_password);
