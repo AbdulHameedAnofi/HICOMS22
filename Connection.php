5f8c34e32a6519b4c36bc172ef9ce360161a84d2
Anofi AbdulHameed <92869818+AbdulHameedAnofi@users.noreply.github.com>
2023-07-20 22:52:15 +0100

    Update Conn

--- Connection.php
+++ Connection.php
@@ -11,7 +11,7 @@ function connection() {
     $cleardb_username = $cleardb_url['user'];
     $cleardb_password = $cleardb_url['pass'];
     $cleardb_db = substr($cleardb_url['path'], 1);
-    // mysql://b790d3e52dd8c0:fbe64c51@eu-cdbr-west-02.cleardb.net/heroku_f658271a9b91c34?reconnect=true
+    
         try {
             $connection = new PDO("mysql:host=$cleardb_server;dbname=$cleardb_db", $cleardb_username, $cleardb_password);
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
